ca6fef5cb168fb6aa07c22b473f57b1000683d12
yanghongyi <yanghongyii@163.com>
2017-06-17 15:46:07 +0800

    placeholder

---
+++ PlaceHoderTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlaceHoderTextView
+//
+//  Created by 太阳在线YHY on 2017/6/17.
+//  Copyright © 2017年 太阳在线. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
